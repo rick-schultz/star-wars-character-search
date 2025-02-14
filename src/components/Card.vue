7a4abd7eba41ae588ae81c809ea4e5a489ec697d
Rick Schultz <rick-schultz@hotmail.com>
2025-02-14 15:56:23 -0300

    feat: create simple card component

---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { Character } from '@/types/character';
+
+const props = defineProps<{
+	character: Character;
+}>();
+
+</script>
+
+<template>
+	<div class="bg-white rounded-xl shadow-md overflow-hidden w-full">
+		<div class="bg-slate-800 p-4">
+			<h2 class="m-0 text-xl font-semibold text-white text-center">
+				{{ props.character.name }}
+			</h2>
+		</div>
+	</div>
+</template>
